52c896670d9e7bc208a46fb7a5c1cc9228d47b42
kwanth <59208523+kwan19961217@users.noreply.github.com>
2021-04-03 23:15:33 +0800

    Update Week1

--- week1/wiki/encyclopedia/urls.py
+++ week1/wiki/encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>", views.title, name="title"),
+    path("search", views.search, name="search"),
+    path("create", views.create, name="create"),
+    path("edit/<str:title>", views.edit, name="edit"),
+    path("random", views.random_search, name="random_search")
 ]
--- week1/wiki/encyclopedia/views.py
+++ week1/wiki/encyclopedia/views.py
@@ -1,10 +1,63 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from . import util
-
+import math
+import random
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def title(request, title):
+    content = util.get_entry(title)
+    if content:
+        return render(request, "encyclopedia/title.html", {
+            "title": title,
+            "content": markdown2.markdown(content)
+        })
+    else:
+        return HttpResponse("<h1>Page Not Found</h1>")
+
+def search(request):
+    title = request.GET['q']
+    content = util.get_entry(title)
+    if content:
+        return redirect("title", title=title)
+    else:
+        fulllist = util.list_entries()
+        searchlist = []
+        for item in fulllist:
+            if title.upper() in item.upper():
+                searchlist.append(item)
+        return render(request, "encyclopedia/search.html", {
+            "entries": searchlist
+        })
+
+def create(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        if title in util.list_entries():
+            return HttpResponse("<h1>Depulicate Title</h1>")
+        else:
+            util.save_entry(title, request.POST['content'])
+            return redirect("title", title=title)
+    else:
+        return render(request, "encyclopedia/create.html")
+
+def edit(request, title):
+    if request.method == "POST":
+        util.save_entry(title, request.POST['content'])
+        return redirect("title", title=title)
+    else:
+        content = util.get_entry(title)
+        return render(request, "encyclopedia/edit.html", {
+            "title": title,
+            "content": content
+        })
+
+def random_search(request):
+    fulllist = util.list_entries()
+    title = (fulllist[math.floor(random.random() * len(fulllist))])
+    return redirect("title", title=title)
